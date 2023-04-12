f986ed96b766517f62b31a50decee25b36997305
Diego-Hinrichs <93550789+Diego-Hinrichs@users.noreply.github.com>
2023-04-12 19:05:09 -0400

    implentacion de componentes
    Manuel Care
    Diego Hinrichs

---
+++ components/VerEgresados.ts
@@ -0,0 +1,18 @@
+import { Egresado } from "./usuario/Egresado"
+
+export class VerEgresados {
+    listarEgresados(): Egresado[] {
+      // implementar la lógica para listar todos los egresados
+      return [];
+    }
+  
+    buscarEgresadoPorCarrera(carrera: string): Egresado[] {
+      // implementar la lógica para buscar egresados por carrera
+      return [];
+    }
+  
+    buscarEgresadoPorAnio(anio: number): Egresado[] {
+      // implementar la lógica para buscar egresados por año de graduación
+      return [];
+    }
+  }
