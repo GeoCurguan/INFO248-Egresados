6379c8a90008cec236fe4dc112c5ffc66cf5636f
alexbgh1 <alex.garnica@alumnos.uach.cl>
2023-06-10 22:02:53 -0400

    Layout de Ruta Protegida, class -> className

--- frontend/src/pages/perfil.jsx
+++ frontend/src/pages/perfil.jsx
@@ -1,36 +1,27 @@
 // Ruta protegida,
 // Valida [auth]
 
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import { useAuthContext } from "../context/MyAuthContext";
 import ProfileSummary from "@/components/profile/ProfileSummary";
+import ProtectedLogged from "@/components/protected/ProtectedLogged";
 
 const Ruta = () => {
-  const { auth, user, handleLogout } = useAuthContext();
-  const router = useRouter();
+  const { user, handleLogout } = useAuthContext();
 
-  useEffect(() => {
-    if (!auth) {
-      router.push("/login");
-    }
-  }, [auth]);
-
-  if (!auth) return null;
-
-  console.log("from Perfil:", user);
   return (
-    <div>
-      <ProfileSummary />
-      <h1>Ruta protegida</h1>
-      <h2>Hola {user.nombres}</h2>
-      <button
-        className="bg-gray-900 text-white p-2 rounded-lg"
-        onClick={handleLogout}
-      >
-        Logout
-      </button>
-    </div>
+    <ProtectedLogged>
+      <div>
+        <ProfileSummary />
+        <h1>Ruta protegida</h1>
+        <h2>Hola {user.nombres}</h2>
+        <button
+          className="bg-gray-900 text-white p-2 rounded-lg"
+          onClick={handleLogout}
+        >
+          Logout
+        </button>
+      </div>
+    </ProtectedLogged>
   );
 };
 
