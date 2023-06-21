548b1e5877361e38da16ce1e8b696b3922c7e326
Alex <alex.garnica@alumnos.uach.cl>
2023-06-21 19:28:45 -0400

    intento rutas dinámicas pero se necesitan endpoints /posts /posts/:id

--- frontend/src/pages/beneficios/index.jsx
+++ frontend/src/pages/beneficios/index.jsx
@@ -1,5 +1,9 @@
+// Import Nextjs
 import { useRouter } from "next/router";
+import Image from "next/image";
+// Import React
 import Hero from "@/components/HeroContent";
+import Link from "next/link";
 
 export default function Home() {
   const router = useRouter();
@@ -18,6 +22,31 @@ export default function Home() {
           title={heroContent.title}
         />
       </main>
+      <div className="block pt-20"></div>
+      <Link href="#">
+        <article
+          className="flex sm:flex-row rounded-md shadow-md flex-col gap-6 mt-10 mx-10 px-5 py-10 bg-gray-200
+        cursor-pointer transition-all hover:bg-gray-300 "
+        >
+          <Image
+            src="/placeholders/home-hero.jpg"
+            alt="Image name"
+            width={200}
+            height={50}
+            className="sm:max-w-md w-full rounded-sm "
+          />
+          <div className="flex flex-col">
+            <h2 className="h-1/3 text-xl">Titulo</h2>
+            <p className="h-2/3 text-md">
+              Slice .200 Lorem ipsum, dolor sit amet consectetur adipisicing
+              elit. Veniam enim consequatur explicabo illum doloremque quidem
+              aperiam natus? Cum accusamus veniam sint molestiae magni quisquam
+              a! Placeat commodi adipisci repellendus ducimus.
+            </p>
+          </div>
+        </article>
+      </Link>
+      <p>ss</p>
     </>
   );
 }
