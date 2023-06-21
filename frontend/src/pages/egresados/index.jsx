19f49a43aee740c227952f02ac80e8288442740b
alexbgh1 <alex.garnica@alumnos.uach.cl>
2023-06-21 11:33:19 -0400

    cambio pequeño fuente y /api/egresados para simular req

--- frontend/src/pages/egresados/index.jsx
+++ frontend/src/pages/egresados/index.jsx
@@ -3,42 +3,19 @@ import FiltroEgresado from "@/components/egresados/FiltroEgresado";
 import CardContent from "@/components/CardContent";
 import SearchBar from "@/components/SearchBar";
 
-const fakeData = [
-  {
-    id: 1,
-    nombres: "Juan",
-    apellidos: "Perez",
-    caracteristicas: "lorem",
-    dominios: "ipsum",
-  },
-  {
-    id: 2,
-    nombres: "Juan",
-    apellidos: "Perez",
-    caracteristicas: "lorem",
-    dominios: "ipsum",
-  },
-  {
-    id: 3,
-    nombres: "Ricardo",
-    apellidos: "Fuentes",
-    caracteristicas: "lorem",
-    dominios: "ipsum",
-  },
-  {
-    id: 4,
-    nombres: "Juan",
-    apellidos: "Perez",
-    caracteristicas: "lorem",
-    dominios: "ipsum",
-  },
-];
-
 const Index = () => {
+  const [egresados, setEgresados] = useState([]);
+
   const [filtros, setFiltros] = useState({
     year: "2023",
     carrera: "Ingeniería en Completos",
   });
+
+  useEffect(() => {
+    fetch("api/egresados")
+      .then((res) => res.json())
+      .then((data) => setEgresados(data));
+  }, []);
 
   useEffect(() => {
     console.log(filtros);
@@ -56,7 +33,7 @@ const Index = () => {
         md:w-3/4
         w-full h-[30rem] bg-gray-200 rounded-md shadow-lg p-4 overflow-auto bar"
         >
-          {fakeData.map((egresado) => (
+          {egresados.map((egresado) => (
             <CardContent
               key={egresado.id}
               tipo={"egresado"}
