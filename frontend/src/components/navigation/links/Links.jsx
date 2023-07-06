ba0989419c240afd5675cfe7503da31b3720fa29
alexbgh1 <alex.garnica@alumnos.uach.cl>
2023-07-06 17:15:40 -0400

    ruta noticias, botones Login/Logout, ruta protegida perfil/:id

--- frontend/src/components/navigation/links/Links.jsx
+++ frontend/src/components/navigation/links/Links.jsx
@@ -2,36 +2,56 @@ import Link from "next/link";
 import { useAuthContext } from "@/context/MyAuthContext";
 
 const navLinks = [
-  { name: "Intereses", href: "/intereses/" },
+  { name: "🏠", href: "/" },
+  { name: "Noticias", href: "/noticias/" },
   { name: "Beneficios", href: "/beneficios/" },
   { name: "Empleos", href: "/empleos/" },
   { name: "Egresados", href: "/egresados/" },
   { name: "Personal", href: "/perfil/" },
 ];
 export const ButtonLogin = () => {
-  return <button>{/* Icon 1 if !login */}login</button>;
+  return (
+    <Link
+      className="block bg-blue-700 hover:bg-blue-800 text-white py-1 px-2 rounded"
+      href={"/login"}
+    >
+      login
+    </Link>
+  );
 };
 
-export const ButtonLogout = () => {
-  return <button>{/* Icon 2 if logut */}logout</button>;
+export const ButtonLogout = ({ handleLogout }) => {
+  return (
+    <button
+      onClick={handleLogout}
+      className="bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded"
+    >
+      logout
+    </button>
+  );
 };
 
 const Links = () => {
-  const { auth } = useAuthContext();
+  const { auth, handleLogout } = useAuthContext();
   console.log("auth from LINKS: ", auth);
 
   return (
     <nav>
-      <ul className="md:flex-nowrap flex flex-wrap justify-center gap-3 py-3 uppercase [&>li:not(last-child)>a]:bg-gray-200 [&>li:not(:last-child)>a]:py-1 [&>li:not(:last-child)>a]:px-5">
+      <ul className="md:flex-nowrap flex flex-wrap justify-center gap-3 py-3 uppercase hover:[&>li:not(:last-child)>a]:bg-gray-300 [&>li:not(:last-child)>a]:bg-gray-200 [&>li:not(:last-child)>a]:py-1 [&>li:not(:last-child)>a]:px-5">
         {navLinks.map((link) => (
           <li key={link.name}>
-            <Link href={link.href}>{link.name}</Link>
+            <Link className="block" href={link.href}>
+              {link.name}
+            </Link>
           </li>
         ))}
         <li>
           {/* Icon 1 if !login */}
-          <button></button>
-          {/* {auth ? <ButtonLogout /> : <ButtonLogin />} */}
+          {auth ? (
+            <ButtonLogout handleLogout={handleLogout} />
+          ) : (
+            <ButtonLogin />
+          )}
           {/* Icon 2 if logut */}
         </li>
       </ul>
